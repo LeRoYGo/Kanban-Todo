10f5079978e47b580e43f360d8702f3fba81a48b
LeRoYGo <roman.lepikhin.2000@gmail.com>
2024-10-01 16:49:59 +0500

    fix

---
+++ src/state.js
@@ -0,0 +1,20 @@
+const INIT_BOARDS = [
+    {
+        id: 1,
+        title: 'to do',
+        tasks: [],
+    },
+    {
+        id: 2,
+        title: 'Doing',
+        tasks: [],
+    },
+    {
+        id: 3,
+        title: 'Done',
+        tasks: [],
+    },
+]
+
+
+export default INIT_BOARDS
